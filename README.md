d6947f3cc6d778a9862944d539c8450d360928fe
Diogo Miranda <diogosminfor@gmail.com>
2021-04-23 10:49:46 -0300

    add projeto final NLW#05 rocketseat

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # podcastr-next
 Projeto de um Podcast feito com Next.js e Typescript durante a NLW5 (https://rocketseat.com.br/)
+Neste projeto busquei praticar um pouco mais sobre o React.js/Next.js e Api.
+Utilizando alguns conceitos básicos do React.js e Next.js, componentes e organização de pastas dentro de um projeto Web Moderno. Além de praticar os conceitos de comunicações com uma API.
+
+# autor
+Feito com muito por [https://diogomiranda.dev.br/](https://diogomiranda.dev.br/)
+Feito em NLW#05 [https://rocketseat.com.br/](rocketseat.com.br) para estudo de react, next, todos os direitos das imagens são da rocketseat.
+
+## APP images
+![Screenshot_1](/img/podcastr_nlw_next_img1.png "Screenshot_1")
+![Screenshot_2](/img/podcastr_nlw_next_img2.png "Screenshot_2")
+
+# GetStart
+### `npm run dev`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to 
+view it in the browser.
+### `npm run build`
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+### `npm run server`
+Runs the app in the development mode.\
+Open [http://localhost:3333](http://localhost:3333) to API Server 
---
+++ src/pages/episodes/[slug].tsx
@@ -0,0 +1,116 @@
+import { useContext } from 'react';
+import { GetStaticProps, GetStaticPaths } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import { usePlayer } from '../../contexts/PlayerContexts';
+import { api } from '../../services/api';
+import { format, parseISO } from 'date-fns';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+import ptBr from 'date-fns/locale/pt-BR';
+import styles from './episode.module.scss';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  thumbnail: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
+};
+
+type EpisodeProps = {
+  episode: Episode;
+};
+
+export default function Episode({ episode }: EpisodeProps) {
+  const { play } = usePlayer();
+
+  return (
+    <div className={styles.episode}>
+      <Head>
+        <title>Podcastr | {episode.title}</title>
+      </Head>
+
+      <div className={styles.thumbnailContainer}>
+        <Link href="/">
+          <button type="button">
+            <img src="/arrow-left.svg" alt="voltar" />
+          </button>
+        </Link>
+        <Image
+          width={700}
+          height={160}
+          src={episode.thumbnail}
+          objectFit="cover"
+        />
+        <button type="button" onClick={() => play(episode)}>
+          <img src="/play.svg" alt="Tocar" />
+        </button>
+      </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+      </header>
+
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: episode.description }}
+      />
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  });
+
+  const paths = data.map((episode) => {
+    return {
+      params: {
+        slug: episode.id,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { slug } = ctx.params;
+
+  const { data } = await api.get(`episodes/${slug}`);
+
+  const episode = {
+    id: data.id,
+    title: data.title,
+    members: data.members,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', {
+      locale: ptBr,
+    }),
+    duration: Number(data.file.duration),
+    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url,
+    thumbnail: data.thumbnail,
+  };
+
+  return {
+    props: { episode },
+    revalidate: 60 * 60 * 24, // 24horas
+  };
+};
